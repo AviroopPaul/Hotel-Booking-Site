8f6b5357029ae11ceeced18f88626e95eef78871
Aviroop Paul <apavirooppaul10@gmail.com>
2022-07-05 12:08:10 +0530

    initial commit

---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose');
+const Scheme= mongoose.Schema;
+
+const CampgroundSchema = new Scheme({
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
